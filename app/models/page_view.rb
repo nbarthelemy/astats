5267253acfb27a28bda810f9e7fbde91bb98bdaf
Nick Barthelemy <nicholas.barthelemy@gmail.com>
2018-09-05 18:02:20 -0500

    Add daily visits query to PageView

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -1,2 +1,17 @@
 class PageView < Sequel::Model
+
+  def self.daily_visits_by_url_since(date)
+    sql = <<-SQL
+      SELECT created_at::DATE, url, count(url) as visits
+      FROM page_views
+      WHERE created_at::DATE > ?
+      GROUP BY created_at::DATE, url
+      ORDER BY created_at::DATE desc, visits desc
+    SQL
+
+    self.db[sql, date].all.group_by do |record|
+      record[:created_at].to_s(:db)
+    end
+  end
+
 end
