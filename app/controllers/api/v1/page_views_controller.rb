66e1c3c8f7fd5f5f9b6122e64089ab320a3cd4fa
Nick Barthelemy <nicholas.barthelemy@gmail.com>
2018-09-05 18:06:39 -0500

    Add caching to page_views index action ( even though this will never go to prod )

--- app/controllers/api/v1/page_views_controller.rb
+++ app/controllers/api/v1/page_views_controller.rb
@@ -2,7 +2,10 @@ module Api::V1
   class PageViewsController < ApiController
 
     def index
-      render json: PageView.daily_visits_by_url_since(5.days.ago.to_date).to_json
+      json = Rails.cache.fetch('page_views_index_json', expires_in: 1.hour) do
+        PageView.daily_visits_by_url_since(5.days.ago.to_date).to_json
+      end
+      render json: json
     end
 
   end
