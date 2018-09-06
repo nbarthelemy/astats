5d66c8a70461cbe51822ad6b8534305609d0dead
Nick Barthelemy <nicholas.barthelemy@gmail.com>
2018-09-05 20:02:32 -0500

    Add tests and new method for PageView ( Assume UTC for everything )

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -1,21 +1,58 @@
 class PageView < Sequel::Model
 
-  def self.daily_visits_by_url_since(date)
+  def self.visits_by_url_since(timestamp)
     sql = <<-SQL
       SELECT created_at::DATE, url, count(url) as visits
       FROM page_views
-      WHERE created_at::DATE > ?
+      WHERE created_at >= ?
       GROUP BY created_at::DATE, url
       ORDER BY created_at::DATE desc, visits desc
     SQL
 
-    self.db[sql, date].all.group_by do |record|
+    self.db[sql, timestamp.utc.to_date].all.group_by do |record|
       record[:created_at].to_s(:db)
     end
   end
 
-  def self.daily_top_five_referrers_by_url_since(since_date)
-    {}
+  def self.top_five_referrers_by_url_since(timestamp)
+    sql = <<-SQL
+      SELECT urls.created_at, urls.url, urls.visits, urls.row_number,
+        referrers.referrer, referrers.referrer_visits, referrers.row_number
+
+      FROM (
+        SELECT created_at::DATE, url, COUNT(url) as visits,
+          ROW_NUMBER() OVER ( PARTITION BY created_at::DATE ORDER BY COUNT(url) DESC )
+        FROM page_views
+        WHERE created_at::DATE >= :since_date::DATE
+        GROUP BY created_at::DATE, url
+      ) AS urls
+
+      INNER JOIN (
+        SELECT created_at::DATE, url, referrer, COUNT(referrer) AS referrer_visits,
+          ROW_NUMBER() OVER ( PARTITION BY url, created_at::DATE ORDER BY COUNT(referrer) DESC )
+        FROM page_views
+        WHERE created_at::DATE >= :since_date::DATE
+        AND referrer IS NOT NULL
+        GROUP BY created_at::DATE, url, referrer
+      ) AS referrers
+
+      ON urls.url = referrers.url
+      AND urls.created_at::DATE = referrers.created_at::DATE
+
+      WHERE urls.row_number <= 10
+      AND referrers.row_number <= 5
+
+      ORDER BY urls.created_at DESC, urls.row_number, url, referrers.row_number
+    SQL
+
+    self.db[sql, { since_date: timestamp.utc.to_date }].all.inject({}) do |data, record|
+      date = record[:created_at].to_s(:db)
+      url = record[:url]
+      data[date] ||= {}
+      data[date][url] ||= { visits: record[:visits], referrers: [] }
+      data[date][url][:referrers] << { url: record[:referrer], visits: record[:referrer_visits] }
+      data
+    end
   end
 
 end
--- test/models/page_view_test.rb
+++ test/models/page_view_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class PageViewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    FixtureDependencies.load(:page_view__apple_google_1_days_ago)
+    FixtureDependencies.load(:page_view__apple_google_2_days_ago)
+    FixtureDependencies.load(:page_view__apple_google_3_days_ago)
+    FixtureDependencies.load(:page_view__apple_google_4_days_ago)
+    FixtureDependencies.load(:page_view__apple_google_5_days_ago)
+  end
+
+  describe "#visits_by_url_since" do
+    it "should return the correctly formatted data structure" do
+      expected_response = (1..5).inject({}) do |response, n|
+        date = n.days.ago.utc.to_date
+        response[date.to_s(:db)] = [{ url: "http://apple.com", visits: 1, created_at: date }]
+        response
+      end
+      PageView.visits_by_url_since(5.days.ago).must_equal expected_response
+    end
+  end
+
+  describe "#top_five_referrers_by_url_since" do
+    it "should return the correctly formatted data structure" do
+      expected_response = (1..5).inject({}) do |response, n|
+        date = n.days.ago.utc.to_date
+        response[date.to_s(:db)] = {
+          "http://apple.com" => {
+            visits: 1,
+            referrers: [{ url: "http://google.com", visits: 1 }]
+          }
+        }
+        response
+      end
+      PageView.top_five_referrers_by_url_since(5.days.ago).must_equal expected_response
+    end
+  end
+
 end
