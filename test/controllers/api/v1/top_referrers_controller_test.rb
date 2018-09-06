7ce3129221306d19ac602d4db3c8de4b1582e792
Nick Barthelemy <nicholas.barthelemy@gmail.com>
2018-09-05 20:10:45 -0500

    Update controller tests

--- test/controllers/api/v1/top_referrers_controller_test.rb
+++ test/controllers/api/v1/top_referrers_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
-describe TopReferrersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+class Api::V1::TopReferrersControllerTest < ActionController::TestCase
+
+  setup do
+    FixtureDependencies.load(:page_view__apple_google_1_days_ago)
+    FixtureDependencies.load(:page_view__apple_google_2_days_ago)
+    FixtureDependencies.load(:page_view__apple_google_3_days_ago)
+    FixtureDependencies.load(:page_view__apple_google_4_days_ago)
+    FixtureDependencies.load(:page_view__apple_google_5_days_ago)
+  end
+
+  test '#index' do
+    get :index
+    assert_response :success
+
+    expected_response = (1..5).inject({}) do |response, n|
+      date = n.days.ago.utc.to_date
+      response[date.to_s(:db)] = {
+        "http://apple.com" => {
+          "visits" => 1,
+          "referrers" => [{ "url" => "http://google.com", "visits" => 1 }]
+        }
+      }
+      response
+    end
+
+    assert_equal expected_response, JSON.parse(@response.body)
+  end
+
 end
